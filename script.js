8d13f6566911ab69205f90d861ce0a9f0610517a
Glitch (image-slideshow-mb) <none>
2020-11-07 17:39:47 +0000

    ☺️🏌 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,24 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
